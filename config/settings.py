f6593aec2488adcb89fdd889f0df1dd4ed26dff9
demetriofr <dmitriy.a.frolov@gmail.com>
2024-03-02 14:08:17 +0300

    Add User model with fields email (unique), phone, city and avatar as well as do migrate

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+#
+# STATICFILES_DIR = (
+#     BASE_DIR / 'static',
+# )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -126,3 +134,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+# For models
+NULLABLE = {'blank': True, 'null': True}
--- users/models.py
+++ users/models.py
@@ -1,5 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from config.settings import NULLABLE
 
 
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='электронная почта')
+
+    phone = models.CharField(max_length=15, verbose_name='номер телефона', **NULLABLE)
+    city = models.CharField(max_length=160, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}: {self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
