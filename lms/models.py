8a68147a749f2071ba5d1b99383e1bae5324d6b6
demetriofr <dmitriy.a.frolov@gmail.com>
2024-03-03 12:29:46 +0300

    Add Lesson model with fields name (unique), description, preview, link_video and course (ManyToMany) as well as migrate.

--- lms/models.py
+++ lms/models.py
@@ -14,3 +14,19 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название', unique=True)
+    preview = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    link_video = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+
+    course = models.ManyToManyField(Course, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
