0b47079516dc604061005e3946ae32138733836f
demetriofr <dmitriy.a.frolov@gmail.com>
2024-03-03 21:10:52 +0300

    Add Serializer and Generic for the Lesson model.

--- lms/urls.py
+++ lms/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 from .apps import LmsConfig
-from .views import CourseViewSet
+from .views import (CourseViewSet, LessonCreateAPIView, LessonListAPIView,
+                    LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView)
 
 app_name = LmsConfig.name
 
@@ -10,5 +12,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lessons/create', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 ] + router.urls
