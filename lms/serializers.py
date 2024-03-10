f625ef893e5ca08309b1f8bf9aaf79b0178d7e8c
demetriofr <dmitriy.a.frolov@gmail.com>
2024-03-11 02:25:02 +0300

    Add count_lessons field in serializer for Course model.

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,10 +4,18 @@ from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    @staticmethod
+    def get_count_lessons(instance):
+        """
+        Return the count of lessons for the given instance.
+        """
+        return instance.lesson_set.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
