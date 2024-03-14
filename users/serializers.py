192ef6a3ab79fa69c733b94729754a0eb4f06370
demetriofr <dmitriy.a.frolov@gmail.com>
2024-03-14 04:35:58 +0300

    Add output list payments for UserSerialiazer.

--- users/serializers.py
+++ users/serializers.py
@@ -3,15 +3,16 @@ from rest_framework import serializers
 from .models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
 class PaymentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Payment
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    payment = PaymentSerializer(source='payment_set', many=True)
+
+    class Meta:
+        model = User
         fields = '__all__'
